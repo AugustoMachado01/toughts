8674a1934ae7159aa350cc21a319d39e5f621562
AugustoMachado01 <augustomachadochieno01@gmail.com>
2024-11-01 17:36:07 -0400

    edited tought

--- controllers/ToughtsController.js
+++ controllers/ToughtsController.js
@@ -17,12 +17,18 @@ module.exports = class ToughtsController {
 
     const toughts = user.Toughts.map((result) => result.dataValues);
 
+    let emptyToughts = false;
+
+    if (toughts.length === 0) {
+      emptyToughts = true;
+    }
+
     //check if user exists
     if (!user) {
       res.redirect("/login");
     }
 
-    res.render("toughts/dashboard", { toughts });
+    res.render("toughts/dashboard", { toughts, emptyToughts });
   }
 
   static async createTought(req, res) {
@@ -63,6 +69,33 @@ module.exports = class ToughtsController {
       });
     } catch (error) {
       console.log("Aconteceu um erro: " + error);
+    }
+  }
+
+  static async UpdateTought(req, res) {
+    const id = req.params.id;
+
+    const tought = await Tought.findOne({ where: { id: id }, raw: true });
+
+    res.render("toughts/edit", { tought });
+  }
+
+  static async UpdateToughtSave(req, res) {
+    const id = req.body.id;
+    const tought = {
+      title: req.body.title,
+    };
+
+    try {
+      await Tought.update(tought, { where: { id: id } });
+
+      req.flash("message", "Pensamento atualizado com sucesso!");
+
+      req.session.save(() => {
+        res.redirect("/toughts/dashboard");
+      });
+    } catch (error) {
+      console.log("deu erro +", error);
     }
   }
 };
--- routes/toughtsRoute.js
+++ routes/toughtsRoute.js
@@ -7,6 +7,8 @@ const checkAuth = require("../helpers/auth").checkAuth;
 
 router.get("/add", checkAuth, ToughtController.createTought);
 router.post("/add", checkAuth, ToughtController.createToughtSave);
+router.get("/edit/:id", checkAuth, ToughtController.UpdateTought);
+router.post("/edit", checkAuth, ToughtController.UpdateToughtSave);
 router.get("/dashboard", checkAuth, ToughtController.Dashboard);
 router.post("/remove", checkAuth, ToughtController.RemoveTought);
 router.get("/", ToughtController.showToughts);
